6efd41ec2af832ec833955eb26e3c5042b89b6fa
katmagic <the.magical.kat@gmail.com>
2010-09-11 12:28:26 -0400

    Add the Connection class to actually connect to the control port.

---
+++ connection.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/ruby
+require 'socket'
+require 'quote'
+
+module RTorCtl
+	class Connection
+		include RTorCtl
+
+		attr_writer :password, :ctlport, :connection
+
+		def initialize(password, ctlport=9051)
+			@password = password
+			@ctlport = ctlport
+		end
+
+		def connect()
+			@connection = TCPSocket.new( "127.0.0.1", @ctlport )
+			authenticate()
+		end
+
+		def quit()
+			self.puts( "QUIT" )
+		end
+
+		def authenticate()
+			self.puts( "AUTHENTICATE #{ quote(@password) }" )
+		end
+
+		def puts(line)
+			self.write( line + "\r\n" )
+		end
+
+		def gets()
+			@connection.gets()
+		end
+
+		def write(data)
+			@connection.write(data)
+		end
+	end
+end
