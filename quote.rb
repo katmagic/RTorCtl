9f2e85155beb2585c76d810b88cdce2b5aac3510
katmagic <the.magical.kat@gmail.com>
2010-09-19 13:01:29 -0400

    Because methods of a module aren't inherited by classes in that module, we have to make RTorCtl.quote() and RTorCtl.unquote() into the Procs RTorCtl::Quote and RTorCtl::Unquote. Grr.

--- quote.rb
+++ quote.rb
@@ -3,11 +3,14 @@
 module RTorCtl
 	QUOTED_CHAR = /[^\x1-\x8\x11\x12\x14-\x31\x127!#-\[\]-~]/
 
-	def quote(str)
+	# These have to be constants because methods in a module are apparently not
+	# inherited by classes in that module. How inelegant. Grr.
+
+	Quote = Proc.new do |str|
 		'"' + str.gsub( /(#{QUOTED_CHAR})/ ){ "\\#{$1}" } + '"'
 	end
 
-	def unquote(str)
+	Unquote = Proc.new do |str|
 		str[1..-2].gsub( /\\(#{QUOTED_CHAR})/ ){$1}
 	end
 end
