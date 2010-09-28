9d64976e7472306d289f3abf9cf52d10eec72078
katmagic <the.magical.kat@gmail.com>
2010-09-28 00:16:30 -0400

    Implement RTorCtl.getconf().

--- getinfo.rb
+++ getinfo.rb
@@ -24,5 +24,30 @@ module RTorCtl
 
 			keywords.length == 1 ? info.first[1] : info
 		end
+
+		def getconf(*keywords)
+			# tor.getconf(:SocksPort) # "9050"
+			# tor.getconf(:SocksPort, :ControlPort)
+			# # {"SocksPort"=>"9050", "ControlPort"=>"9051"}
+
+			@connection.puts("GETCONF #{keywords.join(" ")}")
+			code, response = get_response()
+
+			code.raise()
+
+			if keywords.length == 1
+				return response[0].split("=", 2)[1]
+
+			else
+				results = Hash.new
+
+				response.each do |x|
+					key, value = x.split("=", 2)
+					results[key] = value
+				end
+
+				return results
+			end
+		end
 	end
 end
