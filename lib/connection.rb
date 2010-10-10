9f46065aa316ca92a0598a136e7469bf49f1599c
katmagic <the.magical.kat@gmail.com>
2010-10-10 03:30:04 -0400

    Document Connection.

--- lib/connection.rb
+++ lib/connection.rb
@@ -3,26 +3,32 @@ require 'socket'
 require 'quote'
 
 module RTorCtl
+	# This class holds a connection to Tor's control port.
 	class Connection
 		def initialize(ctlport=9051)
 			@ctlport = ctlport
 		end
 
+		# Make the connection to the control port.
 		def connect()
 			@connection = TCPSocket.new( "127.0.0.1", @ctlport )
 		end
 
+		# Write a line to Tor.
 		def puts(line)
 			self.write( line + "\r\n" )
 			STDOUT.puts("C: #{line.inspect}") if $DEBUG
 		end
 
+		# Retrieve a string from Tor terminated with +sep+.
+		# @return [String] the recieved string with +sep+ removed
 		def gets(sep="\r\n")
 			s = @connection.gets(sep)
 			return nil unless s
 			s[0...-sep.length].tap{|x| STDOUT.puts("S: #{x.inspect}") if $DEBUG}
 		end
 
+		# Write raw data to Tor.
 		def write(data)
 			@connection.write(data)
 		end
