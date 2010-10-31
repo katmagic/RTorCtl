dced738cf0bac38b01fecbda2073b63369523f65
katmagic <the.magical.kat@gmail.com>
2010-10-31 15:45:13 -0400

    Add the DelayedResult class.

--- lib/events.rb
+++ lib/events.rb
@@ -2,10 +2,12 @@
 require 'set'
 
 module RTorCtl
-	# Call a method after a method of a proxied object is called.
-	class CallerProxy
+	class BasicObject
 		undef_method *(instance_methods - ['__send__', '__id__'])
+	end
 
+	# Call a method after a method of a proxied object is called.
+	class CallerProxy < BasicObject
 		# +hook+ will be called with +proxied_object+ as the sole argument every
 		# time a method is called.
 		def initialize(proxied_object, &hook)
@@ -21,6 +23,19 @@ module RTorCtl
 
 		def __wrapped
 			@obj
+		end
+	end
+
+	# Give a block to be evaluated lazily.
+	class DelayedResult < BasicObject
+		def initialize(&block)
+			@block = block
+		end
+
+		def method_missing(m, *a, &b)
+			@obj = @block.call() unless defined? @obj
+
+			@obj.__send__(m, *a, &b)
 		end
 	end
 
