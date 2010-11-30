e7a70f44ce247c45c8575f015ef2e08e79ddf490
katmagic <the.magical.kat@gmail.com>
2010-11-29 22:23:20 -0500

    Move Bytes to a separate file.

--- lib/relay.rb
+++ lib/relay.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/ruby
 require 'exit_policy'
+require 'bytes'
 require 'rubygems'
 require 'only_once'
 require 'time'
@@ -42,47 +43,6 @@ module RTorCtl
 			@line = line
 
 			super( "we don't know about #{option}" )
-		end
-	end
-
-	# This class represents size in a user friendly way.
-	#
-	# @example
-	#  [141622, 142978, 4723985].map{|x| RTorCtl::Bytes.new(x).to_s}
-	#  # ["138.30KB", "139.63KB", "4.51MB"]
-	class Bytes
-		# @param [Fixnum] bytes
-		def initialize(bytes)
-			@i = bytes
-		end
-
-		def  b() @i / 1024.0**0 end
-		def kb() @i / 1024.0**1 end
-		def mb() @i / 1024.0**2 end
-		def gb() @i / 1024.0**3 end
-		def tb() @i / 1024.0**4 end
-
-		# @return [String] a string like "138.30KB" or "4.51MB"
-		def to_s
-			%w{ tb gb mb kb b }.each do |x|
-				if send(x) > 1
-					return "%.2f#{x.upcase}" % send(x)
-				end
-			end
-
-			return "0B"
-		end
-
-		def to_i
-			@i
-		end
-
-		def ==(x)
-			@i == x
-		end
-
-		def method_missing(meth, *args, &block)
-			self.class.new( @i.send(meth, *args, &block) )
 		end
 	end
 
