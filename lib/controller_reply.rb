a7b368616f8cc57f5b99e87e06a8297df2b2ada6
katmagic <the.magical.kat@gmail.com>
2011-08-26 23:34:56 -0400

    Add the ControllerReply grammar.

---
+++ lib/controller_reply.rb
@@ -0,0 +1,93 @@
+module RTorCtl
+	class GenericReply
+		def has_data?
+			case @data.length
+				when 0 then false
+				when 1 then true
+				else :many
+			end
+		end
+
+		# If as_array is true, our return value will be an Array of Strings. If
+		# as_array if false, data will be a String, and an exception will be
+		# raised if multiple data values, or no data values at all, were given.
+		# Otherwise, we will return an Array if multiple values were given, a
+		# String if only one value was given, or nil if no values were given.
+		def data(as_array=nil)
+			if as_array == true
+				@data
+			elsif as_array == false
+				case @data.length
+					when 0
+						raise "no data was provided"
+					when 1
+						@data.first
+					else
+						raise "multiple data values were provided"
+				end
+			elsif as_array == nil
+				case @data.length
+					when 0 then nil
+					when 1 then @data.first
+					else @data
+				end
+			end
+		end
+
+		attr_reader :status_code
+
+		# lines is an Array of Strings containing the replies given by the
+		# controller (excluding the data). They do not contain CRLFs.
+		attr_reader :lines
+
+		# We return our first line, but raise an exception if we have more than one.
+		def value
+			if @lines.length == 1
+				@lines.first
+			else
+				raise "value was called when there were multiple lines"
+			end
+		end
+
+		# We should only be initialized from the ControllerReply grammar. lines is
+		# an Array of *_reply_lines. data_reply_lines should have a data attribute
+		# containing their data, and all reply lines should have a status_code
+		# attribute containing their (integer) status code. *_reply_lines should not
+		# contain their status code, nor should they contain a CRLF.
+		def initialize(lines)
+			@data = Array.new
+			@lines = Array.new
+
+			lines.each do |l|
+				if l.has_data?
+					@data << l.data
+				end
+
+				if @status_code and @status_code != l.status_code.to_i
+					raise "incongruent status codes"
+				elsif not @status_code
+					@status_code = l.status_code.to_i
+				end
+
+				@lines << l.value
+			end
+
+			@lines.freeze
+			@data.freeze
+		end
+	end
+
+	class ReplyLine < Struct.new(:status_code, :value, :data)
+		def has_data?
+			data != nil
+		end
+	end
+
+	module ReplyLineInclude
+		def value
+			ReplyLine.new(status_code.to_i, reply_line.value,
+			              data && data.value).freeze
+		end
+	end
+end
+
---
+++ tests/controller_reply_test.rb
@@ -0,0 +1,121 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+require 'bundler/setup'
+require 'citrus_test'
+
+GrammarTest.grammar_dir = "../grammars"
+
+class TestControllerReply < GrammarTest
+	SHIT = [
+		"Hello, world!",
+		"I♥UTF-8",
+		"Newlines!!!" + ("\n" * 5000) + "T~I~L~D~E",
+		"CRLFs!!!" + ("\r\n" * 5000) + "yo!",
+		"\r\n.\r\x00\n\r\n",
+		"",
+		"¡Ruby Rocks!\r",
+		"hello",
+		"4200",
+		"420A",
+		"A420",
+		"4\r\n20",
+		"420\n",
+		"420 ",
+		"42",
+		"4",
+		"-1"
+	]
+
+	def test_data
+		SHIT.each do |d|
+			assert_parses(:data, d + "\r\n.\r\n", d)
+			deny_parses(:data, d)
+		end
+	end
+
+	def test_status_code
+		1000.times do |i|
+			assert_parses(:status_code, "%03d" % i, i)
+		end
+
+		SHIT.each do |s|
+			deny_parses(:status_code, s)
+		end
+	end
+
+	def test_reply_line
+		SHIT.reject(&/\r\n/.method(:match)).each do |s|
+			assert_parses(:reply_line, s + "\r\n", s)
+			deny_parses(:reply_line, s)
+		end
+	end
+
+	def test_data_reply_line
+		SHIT.reject(&/\r\n/.method(:match)).each do |s|
+			line = entropy( rand(1024) ).gsub("\r\n", 'i♥ruby')
+			data = entropy( rand(4096) ).gsub("\r\n.\r\n", 'i♥ruby')
+			status_code = rand(1000)
+
+			assert_parses(:data_reply_line,
+			              "%03d+%s\r\n%s\r\n.\r\n" % [status_code, line, data],
+			              value: line, data: data, status_code: status_code)
+		end
+
+		SHIT.each do |s|
+			deny_parses(:data_reply_line, s)
+		end
+	end
+
+	{'mid' => '-', 'end' => ' '}.each do |line_type, sep|
+		define_method("test_#{line_type}_reply_line") do
+			{
+				"060#{sep}Hi" => 60,
+				"181#{sep}YYYYYYYYYYYYAO" => 181,
+				"160#{sep}5 ☹ ♥ Æ ĸ ¸" => 160,
+				"700#{sep}\n\n\r\r\r\r \nSPLEH" => 700,
+				"000#{sep} " => 0,
+				"991#{sep}kreiti" + ("\r"*500) + "n" + ("\n" * 500) => 991,
+				"051#{sep}" => 51
+			}.each do |str, status_code|
+				assert_parses("#{line_type}_reply_line", str + "\r\n",
+				              value: str[4..-1], status_code: status_code)
+			end
+
+			SHIT.each do |s|
+				deny_parses("#{line_type}_reply_line", s)
+			end
+		end
+	end
+
+	def test_reply
+		{
+			"501 zed\r\n" => [501, %w{zed}, nil],
+			"666-satan\r\n666 lucifer\r\n" => [666, %w{satan lucifer}, nil],
+			"123+A\r\nDATA\r\n.\r\n123 B\r\n" => [123, %w{A B}, "DATA"],
+			"000+hello\r\ngoodbye\r\n.\r\n000 !\r\n" => [0, %w{hello !}, "goodbye"],
+			"151+bye\r\nhi\r\n\r\n.\r\n151 ¡\r\n" => [151, %w{bye ¡}, "hi\r\n"],
+			"777+god\r\n\r\n.\r\n777 damn\r\n" => [777, %w{god damn}, ""],
+			"011+\r\n\r\n.\r\n011 \r\n" => [11, ["", ""], ""],
+			"189 \r\n" => [189, [""], nil],
+			"001+A\r\nB\r\n.\r\n001+C\r\nD\r\n.\r\n001 E\r\n" =>
+				[1, %w{A C E}, %w{B D}],
+			"110+L1\r\nD1\r\n.\r\n110+L2\r\n\r\nD2\r\n.\r\n110 L3\r\n" =>
+				[110, %w{L1 L2 L3}, ["D1", "\r\nD2"]]
+		}.each do |str, (status, lines, data)|
+			assert_parses(:reply, str, status_code: status, lines: lines, data: data)
+		end
+
+		SHIT.each do |s|
+			deny_parses(:reply, s)
+		end
+	end
+
+	private
+
+	# Get count bytes of UTF-8 encoded entropy.
+	def entropy(count)
+		@devurand ||= open("/dev/urandom")
+		@devurand.read(count).encode("UTF-8", undef: :replace, invalid: :replace)
+	end
+end
+
