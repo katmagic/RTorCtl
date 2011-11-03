073700af175c88d346400c07f4d260430ffd4354
katmagic <the.magical.kat@gmail.com>
2011-11-03 19:53:16 -0400

    Make our quoting tests pass again.

--- tests/quoting_test.rb
+++ tests/quoting_test.rb
@@ -9,22 +9,22 @@ GrammarTest.grammar_dir = "../grammars"
 
 class TestDoubleQuotedString < GrammarTest
 	def test_unquoted_char
-		%w{' a b c d e - : ^ ( ! . }.each do |c|
+		%W{\x27 a b c d e - : ^ ( ! . }.each do |c|
 			assert_parses(:unquoted_char, c, c)
 		end
 
-		%W{♥ € \\ " \n \ « ¡ \0 hi two for-}.each do |c|
+		%W{♥ € \\ \x22 \n \ « ¡ \0 hi two for-}.each do |c|
 			deny_parses(:unquoted_char, c)
 		end
 		deny_parses(:unquoted_char, '')
 	end
 
 	def test_quoted_char
-		%w{\♥ \" \\\\ \¡ \« \¸ \ñ \¯ \⁺}.each do |c|
+		%W{\\♥ \\\x22 \\\\ \\¡ \\« \\¸ \\ñ \\¯ \\⁺}.each do |c|
 			assert_parses(:quoted_char, c, c[1])
 		end
 
-		%w{♥ \k k ¡♥♥¡ po!* 8€ \\}.each do |c|
+		%w{♥ \\k k ¡♥♥¡ po!* 8€ \\}.each do |c|
 			deny_parses(:quoted_char, c)
 		end
 		deny_parses(:quoted_char, '')
@@ -32,13 +32,13 @@ class TestDoubleQuotedString < GrammarTest
 
 	def test_qatom
 		{
-			'\♥' => '♥', '\⁽' => '⁽', '\⚧' => '⚧', '\⚳' => '⚳', '\⚑' => '⚑',
-			'k' => 'k', 'P' => 'P', "\\\0" => "\0", "\\\n" => "\n", '\«' => '«'
+			'\\♥' => '♥', '\\⁽' => '⁽', '\\⚧' => '⚧', '\\⚳' => '⚳', '\\⚑' => '⚑',
+			'k' => 'k', 'P' => 'P', "\\\0" => "\0", "\\\n" => "\n", '\\«' => '«'
 		}.each do |u, p|
 			assert_parses(:qatom, u, p)
 		end
 
-		%w{hi ho ♥ \k \m \¡¡ -- ¡\¸ °ĸ ° ja«l &$ —— :' *% -k \o \m \p}.each do |c|
+		%w{hi ho ♥ \\k \\m \\¡¡ -- ¡\\¸ °ĸ ° ja«l &$ —— *% -k \\o \\m}.each do |c|
 			deny_parses(:qatom, c)
 		end
 		deny_parses(:qatom, '')
@@ -46,17 +46,17 @@ class TestDoubleQuotedString < GrammarTest
 
 	def test_qcontent
 		{
-			'\¡Oh Em Gee!' => '¡Oh Em Gee!',
-			'\"infinity\°F' => '"infinity°F',
+			'\\¡Oh Em Gee!' => '¡Oh Em Gee!',
+			'\\"infinity\\°F' => '"infinity°F',
 			'avalanche' => 'avalanche',
-			'\"posthumous groundhog\"' => '"posthumous groundhog"',
-			'\…\…\…\…\…' => '……………',
+			'\\"posthumous groundhog\\"' => '"posthumous groundhog"',
+			'\\…\\…\\…\\…\\…' => '……………',
 			'' => ''
 		}.each do |u, p|
 			assert_parses(:qcontent, u, p)
 		end
 
-		%w{♥sweet♥ áĺíéńź hea\rt kĸ- meg\ son"ic tomb¡}.each do |s|
+		%w{♥sweet♥ áĺíéńź hea\\rt kĸ- meg\\ son\\x22ic tomb¡}.each do |s|
 			deny_parses(:qcontent, s)
 		end
 	end
@@ -64,9 +64,9 @@ class TestDoubleQuotedString < GrammarTest
 	def test_string
 		{
 			'"hello, world!"' => 'hello, world!',
-			'"\¡Adios, mundo!"' => '¡Adios, mundo!',
-			'"\"kremey"' => '"kremey',
-			'"\♥"' => '♥',
+			'"\\¡Adios, mundo!"' => '¡Adios, mundo!',
+			'"\\"kremey"' => '"kremey',
+			'"\\♥"' => '♥',
 			'""' => ''
 		}.each do |u, p|
 			assert_parses(:string, u, p)
